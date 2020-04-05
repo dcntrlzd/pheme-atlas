46119daa2de8e41c6a938f45f9991b26550ee44b
Baris Gümüstas <barisgumustas@gmail.com>
2020-04-05 21:41:35 +0200

    Improving raspberry setup script

--- compose/pi/prepare.sh
+++ compose/pi/prepare.sh
@@ -1,6 +1,14 @@
-curl -sSL https://get.docker.com | sh
-usermod -aG docker pi
-docker info -f "{{.Name}}"
-apt-get install -y python3 python3-pip
-pip3 install docker-compose
-mkdir /var/ipfs
+#!/bin/bash
+
+if ! [ -x "$(command -v docker)" ]; then
+  curl -sSL https://get.docker.com | sh
+  usermod -aG docker pi
+  docker info -f "{{.Name}}"
+fi
+
+if ! [ -x "$(command -v pip3)" ]; then
+  apt-get install -y python3 python3-pip
+  pip3 install docker-compose
+fi
+
+mkdir -p /var/ipfs
